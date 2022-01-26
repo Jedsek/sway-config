81e42197eb8ad02546e4e016a205f098ea8c8477
Jedsek <63626406+Jedsek@users.noreply.github.com>
2022-01-26 14:27:00 +0800

    Update config.sh

--- window-managers/config.sh
+++ window-managers/config.sh
@@ -1,9 +1,33 @@
 #!/usr/bin/env bash
-path=~/sway-config
-for file in $path/window-managers/*
-do
-  if [ -d "$file" ]
-  then
-    sh $file/config.sh
-  fi
-done
+
+echo "============================================================================================================================================================"
+echo "[SCRIPT] 正在配置sway-wm"
+
+path=~/sway-config/window-managers/sway
+
+## Install some tools
+yes " " | yay -S otf-font-awesome
+yes " " | yay -S sway swaylock brightnessctl
+
+
+## Sway
+mkdir -p ~/.config/sway/wallpapers
+cp -r  $path/wallpapers/  ~/.config/sway/
+cp     $path/config       ~/.config/sway/
+
+
+## Plugins
+# autotiling-rs
+sh $path/scripts/config.sh
+
+
+## Waybar
+sh $path/waybar/config.sh
+
+
+## Mako
+sh $path/mako/config.sh
+
+
+## Wofi
+sh $path/wofi/config.sh
