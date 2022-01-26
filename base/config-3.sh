cf12e7ed19bc748c663b757f75763aadd8de8a30
Jedsek <63626406+Jedsek@users.noreply.github.com>
2022-01-26 17:18:42 +0800

    Update config-3.sh

--- base/config-3.sh
+++ base/config-3.sh
@@ -3,6 +3,6 @@ echo "[SCRIPT] 正在安装常用工具, npm, nodejs, hexo, vlc, tree, fd, bat, 
 yes " " | yay -S npm nodejs
 npm config set registry https://registry.npm.taobao.org
 sudo npm install -g npm hexo
-yes " " | yay -S vlc mpv tree fd bat exa ripgrep 
+yes " " | yay -S vlc mpv tree fd bat exa ripgrep axel
 yes " " | yay -S neofetch feh geary xdotool
 yes " " | yay -S steam dingtalk
