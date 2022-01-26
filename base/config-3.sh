7e88135e9394d2568e054c0400dff1db46142717
Jedsek <63626406+Jedsek@users.noreply.github.com>
2022-01-26 14:17:52 +0800

    Update config-3.sh

--- base/config-3.sh
+++ base/config-3.sh
@@ -4,5 +4,5 @@ yes " " | yay -S npm nodejs
 npm config set registry https://registry.npm.taobao.org
 sudo npm install -g npm hexo
 yes " " | yay -S vlc mpv tree fd bat exa ripgrep 
-yes " " | yay -S neofetch feh geary
+yes " " | yay -S neofetch feh geary xdotool
 yes " " | yay -S steam dingtalk
