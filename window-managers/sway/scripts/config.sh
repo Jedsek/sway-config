c52b0fcd0b467a7acff9762407a9ce6dc57c9c6f
Jedsek <63626406+Jedsek@users.noreply.github.com>
2022-01-26 18:16:14 +0800

    Update config.sh

--- window-managers/sway/scripts/config.sh
+++ window-managers/sway/scripts/config.sh
@@ -4,9 +4,7 @@ yes " " | yay -S wf-recorder
 
 path=~/.config/sway/scripts
 
-mkdir $path/
-cp    ~/sway-config/window-managers/sway/scripts/ ~/.config/sway/
+cp -r ~/sway-config/window-managers/sway/scripts/ ~/.config/sway/
 rm    $path/config.sh
 
 chmod +x $path/autotiling-rs
-
