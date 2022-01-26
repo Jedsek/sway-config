658f9418e553aab8c3f0cda2d3e3c9553ab5b659
Jedsek <63626406+Jedsek@users.noreply.github.com>
2022-01-26 15:38:25 +0800

    Update config.sh

--- window-managers/sway/wofi/config.sh
+++ window-managers/sway/wofi/config.sh
@@ -2,6 +2,4 @@
 
 yes " " | yay -S wofi
 
-mkdir ~/.config/wofi
-cp ~/sway-config/window-managers/sway/wofi/config     ~/.config/wofi/
-cp ~/sway-config/window-managers/sway/wofi/style.css  ~/.config/wofi/
+cp ~/sway-config/window-managers/sway/wofi  ~/.config/
