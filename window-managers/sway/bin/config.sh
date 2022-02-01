05554250d9255fe10b337b9581f72d6ff261ada9
Jedsek <63626406+Jedsek@users.noreply.github.com>
2022-02-01 16:13:59 +0800

    Update config.sh

--- window-managers/sway/bin/config.sh
+++ window-managers/sway/bin/config.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 path=~/.config/sway/bin
 mkdir $path
-axel -n 200 -o $path/DevSidecar https://gh.api.99988866.xyz/https://github.com/docmirror/dev-sidecar/releases/download/v1.7.2/DevSidecar-1.7.2.AppImage
+axel -n 200 -o $path/DevSidecar.AppImage https://gh.api.99988866.xyz/https://github.com/docmirror/dev-sidecar/releases/download/v1.7.2/DevSidecar-1.7.2.AppImage
