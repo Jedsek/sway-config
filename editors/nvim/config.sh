bf12c36fb9a8d52aba261c7e9f3471ffbb76387a
Jedsek <63626406+Jedsek@users.noreply.github.com>
2022-02-04 21:50:44 +0800

    Update config.sh

--- editors/nvim/config.sh
+++ editors/nvim/config.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 yes " " | yay -S neovim
 path=~/.config/nvim
+git clone --depth 1 https://github.com.cnpmjs.org/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 git clone https://github.com.cnpmjs.org/jedsek/nvim-config $path
